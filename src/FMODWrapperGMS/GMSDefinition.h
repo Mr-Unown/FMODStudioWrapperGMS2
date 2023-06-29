da50101fe7165cd283a8374b61e69bbe37003fe1
Mr. Unown [Demo 3 Guy] <80027199+Mr-Unown@users.noreply.github.com>
2023-06-29 10:27:06 +0800

    FMOD Studio DLL Src

---
+++ src/FMODWrapperGMS/GMSDefinition.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GM_FUNC extern "C" __declspec (dllexport)
+
+#define GM_true 1
+#define GM_false 0
+#define GM_error -1;
+#define GM_noone -4;
+
+double FMODGMS_Util_ErrorChecker();
+
---
+++ src/FMODWrapperGMS/framework.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <fmod.hpp>
+#include <fmod_studio.hpp>
+#include <fmod_errors.h>
+
+/*
+typedef enum FMOD_BANK
+{
+	FMOD_MASTER,
+	FMOD_MASTER_STRINGS,
+	FMOD_MUSIC,
+	FMOD_SFX
+} FMOD_BANK;
+*/
